d26e4486b594ea72eb83c325782c48d6b71ef268
Furkan Kıraç <fkirac@gmail.com>
2018-02-09 17:06:21 +0300

    Allocate memory...

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -13,6 +13,21 @@ int main(int argc, char* argv[])
     Data* pData = (Data*)malloc(sizeof(Data));
     printf("I did it\n");
 
+    pData->age = 40;
+
+    pData->name = (char*)malloc(10);
+
+    pData->name[0] = 'f';
+    pData->name[1] = 'u';
+    pData->name[2] = 'r';
+    pData->name[3] = 'k';
+    pData->name[4] = 'a';
+    pData->name[5] = 'n';
+    pData->name[6] = '\0';
+
+    printf("The string I created is %s.\n", pData->name);
+
+    free(pData->name);
     free(pData);
     return 0;
 }
