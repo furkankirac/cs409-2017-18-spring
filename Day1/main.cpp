b66fcc97b0fae1d863dd3a8166c7294910f22b69
Furkan Kıraç <fkirac@gmail.com>
2018-02-09 17:15:12 +0300

    Some archaic C++ flavour.

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -5,29 +5,36 @@ struct Data
 {
     int age;
     char* name;
+
+    Data()
+    {
+        age = 40;
+
+        name = (char*)malloc(10);
+        name[0] = 'f';
+        name[1] = 'u';
+        name[2] = 'r';
+        name[3] = 'k';
+        name[4] = 'a';
+        name[5] = 'n';
+        name[6] = '\0';
+    }
+
+    ~Data()
+    {
+        free(name);
+    }
 };
 
 int main(int argc, char* argv[])
 {
     printf("I'm gonna allocate %ld bytes.\n", sizeof(Data));
-    Data* pData = (Data*)malloc(sizeof(Data));
+
+    Data* pData = new Data();
     printf("I did it\n");
-
-    pData->age = 40;
-
-    pData->name = (char*)malloc(10);
-
-    pData->name[0] = 'f';
-    pData->name[1] = 'u';
-    pData->name[2] = 'r';
-    pData->name[3] = 'k';
-    pData->name[4] = 'a';
-    pData->name[5] = 'n';
-    pData->name[6] = '\0';
 
     printf("The string I created is %s.\n", pData->name);
 
-    free(pData->name);
-    free(pData);
+    delete pData;
     return 0;
 }
