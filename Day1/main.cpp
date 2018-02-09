b57bcb6be9d67a560d44e463f69ae1f7c002d845
Furkan Kıraç <fkirac@gmail.com>
2018-02-09 16:43:19 +0300

    Old C way of reading console arguments.

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    printf("Hello World! I have %d arguments.\n", argc);
+
+    for(int i=0; i<argc; ++i)
+        printf(">>> %s\n", argv[i]);
+
     return 0;
 }
