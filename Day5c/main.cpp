3b12dd28befc96bed74ed0ae28bd5cbdb133aa05
Furkan Kıraç <fkirac@gmail.com>
2018-03-08 18:38:44 +0300

    function pointers, member function pointers, functors, std::function, and template based lambda captures.

--- Day5c/main.cpp
+++ Day5c/main.cpp
@@ -1,25 +1,94 @@
 #include <iostream>
+#include <functional>
 
-using namespace std;
+int mymin(int a, int b)
+{
+    return a < b ? a : b;
+}
 
-// global functions, member functions, static functions, friend functions, inline functions
-// global variables, static variables
-// function pointers, member function pointers, std::function
-// functors, function pointers and lambdas as arguments
-// capturing return variable's type
-
-inline int max(int a, int b)
+int mymax(int a, int b)
 {
     return a > b ? a : b;
+}
+
+struct Dummy
+{
+    int value;
+
+    void foo()
+    {
+        std::cout << "foo = " << value << std::endl;
+    }
+};
+
+struct Functor
+{
+    inline int operator()(int a, int b) const
+    {
+        return a > b ? a : b;
+    }
+};
+
+template <typename FUNC>
+int doIt_fast(int a, int b, const FUNC& func)
+{
+    return func(a, b);
+}
+
+
+int doIt_slow(int a, int b, std::function<int(int, int)> func)
+{
+    return func(a, b);
 }
 
 
 int main()
 {
-    int a = 3;
-    int b = 5;
-    cout << max(a, b) << endl;
-    cout << (a > b ? a : b) << endl;
+    // function pointer definitions
+    {
+//        int(*func)(int, int) = mymax; // direct style variable declaration
+//        typedef int(*func_ptr)(int, int); // old style type definition
+        using func_ptr = int(*)(int, int); // new style type definition
+        func_ptr func = mymax;
+        std::cout << func(3, 5) << std::endl;
+    }
+
+    std::cout << doIt_slow(3, 5, [](int a, int b) { return a > b ? a : b; }) << std::endl;
+
+    // assigning a functor to std::function and runtime calling of the lambda by a function call
+    {
+        Functor functor;
+        std::function<int(int, int)> func = functor;
+        std::cout << doIt_slow(3, 5, func) << std::endl;
+    }
+
+    // assigning a lambda to std::function and runtime calling of the lambda by a function call
+    {
+        std::function<int(int, int)> func = [](int a, int b) { return a > b ? a : b; };
+        std::cout << doIt_slow(3, 5, func) << std::endl;
+    }
+
+    // compile time binding of lambda -> more optimization, no function call for evaluating max
+    std::cout << doIt_fast(3, 5, [](int a, int b) { return a > b ? a : b; }) << std::endl;
+
+    // functor is already instantiated, there is a function belonging to it,
+    // no way of optimization even if we call fast version
+    // fast version acts as if it is a slow version. It will call the function in functor.
+    {
+        Functor functor;
+        std::cout << doIt_fast(3, 5, functor) << std::endl;
+    }
+
+    // member function pointer usage
+    {
+        using memfunc_ptr = void(Dummy::*)();
+
+        Dummy d{8};
+        d.foo();
+
+        memfunc_ptr mfunc = &Dummy::foo;
+        (d.*mfunc)();
+    }
 
     return 0;
 }
