c9d5591d00614d8f80cc6dc94ba311f05caeef51
Furkan Kıraç <fkirac@gmail.com>
2018-03-15 13:52:43 +0300

    printVector is templatized.

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -17,19 +17,56 @@
 // std::function and member function pointer storage
 // compiler explorer: -O3
 
-void printVector(const std::vector<int>& vec)
+namespace X
+{
+    template<typename T>
+    struct vec : public std::vector<T>
+    {
+        using base = std::vector<T>;
+        using base::base;
+        using base::operator =;
+    };
+
+} // end of X
+
+
+template<typename T>
+void printVector(const std::vector<T>& vec)
 {
     for(auto value : vec)
-        std::cout << value << std::endl;
+    {
+        if constexpr(std::is_same<T, std::string>::value)
+            std::cout << "'" << value << "'" << std::endl;
+        else
+            std::cout << value << std::endl;
+    }
 }
+
+void convert(std::vector<std::string>& to, const std::vector<int>& from)
+{
+    to.clear();
+    for(const auto a : from)
+        to.push_back(std::to_string(a));
+}
+
 
 int main(int argc, char* argv[])
 {    
     using namespace std;
 
-    int array[10];
+    std::vector<int> A{1, 2, 3, 4};
+    std::vector<std::string> B;
+    std::vector<float> C{1.1f, 2.2f, 3.3f, 4.4f};
 
-    vector<int> v{10, 20};
-    printVector(v);
+    convert(B, A);
+//    convert(B, std::vector<int>{1, 2, 3, 4}); // rvalue, this works because of const usage in convert
+    printVector(A);
+    printVector(B);
+    printVector(C);
+
+//    B = A;
+
+//    X::vec<int> v{10, 20};
+//    printVector(v);
 
 }
