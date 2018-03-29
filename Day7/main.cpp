538865157296388b721197ebb8e58f0bb0d7d9ac
Furkan Kıraç <fkirac@gmail.com>
2018-03-29 13:58:03 +0300

    ...

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -17,25 +17,20 @@
 // std::any
 // [[fallthrough]], [[maybe_unused]], [[nodiscard]]
 
-
 int main()
 {
     using namespace std;
 
-    std::variant<int, float, long double, std::string> dummy;
-    dummy = 5;
-    dummy = 5.3f;
-    dummy = 10.0l;
-    dummy = "furkan";
+    pair<int, float> p{3, 3.14f};
+    cout << "First=" << p.first << ", Second=" << p.second << endl;
 
-    try
-    {
-        auto value = std::get<int>(dummy);
-    }
-    catch(const std::bad_variant_access& e)
-    {
-        cout << "Error" << endl;
-    }
+    tuple<double, std::string, float> t{1.2, "furkan"s, 10.0f};
+    auto value = std::get<0>(t);
+    auto value2 = std::get<1>(t);
+    auto other = std::get<double>(t);
+
+    cout << value << endl;
+    cout << value2 << endl;
 
     return 0;
 }
