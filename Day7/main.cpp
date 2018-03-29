57680e0db614594dd9a41147896941eca22b8eac
Furkan Kıraç <fkirac@gmail.com>
2018-03-29 14:02:41 +0300

    Returning brace initialized list.

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -3,7 +3,6 @@
 #include <array>
 #include <variant>
 
-// std::array, std::pair, std::tuple
 // returning by copy list initialization
 // structured bindings
 // generic lambdas
@@ -17,20 +16,22 @@
 // std::any
 // [[fallthrough]], [[maybe_unused]], [[nodiscard]]
 
+struct Dummy
+{
+    std::string s;
+    int k;
+};
+
+Dummy func()
+{
+    return {"furkan", 5};
+}
+
+
 int main()
 {
     using namespace std;
 
-    pair<int, float> p{3, 3.14f};
-    cout << "First=" << p.first << ", Second=" << p.second << endl;
-
-    tuple<double, std::string, float> t{1.2, "furkan"s, 10.0f};
-    auto value = std::get<0>(t);
-    auto value2 = std::get<1>(t);
-    auto other = std::get<double>(t);
-
-    cout << value << endl;
-    cout << value2 << endl;
 
     return 0;
 }
