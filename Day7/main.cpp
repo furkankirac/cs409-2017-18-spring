429a5058e628764519cc139d4806088047f4a35b
Furkan Kıraç <fkirac@gmail.com>
2018-03-29 12:54:13 +0300

    Init statement requirement for if.

---
+++ Day7/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <variant>
+
+// clang code-model
+
+// init-statement for if/switch
+
+// initializer lists
+// std::array, std::pair, std::tuple
+// returning by copy list initialization
+// structured bindings
+// generic lambdas
+// union, std::variant
+// std::any
+// std::optional
+// static_cast, dynamic_cast, reinterpret_cast, const_cast
+
+// template specialization
+// inline variables
+// returning {}
+// std::any
+// [[fallthrough]], [[maybe_unused]], [[nodiscard]]
+
+int main()
+{
+    using namespace std;
+
+    auto longJob = [] {
+        // ... execute some long jobs
+        return 15;
+    };
+
+    {
+        auto value = longJob();
+        if(value > 10)
+        {
+            // do some other things
+            float k = value * 10;
+            // do other things with k
+        }
+    }
+
+
+
+    return 0;
+}
