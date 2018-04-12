48b91d49da37c31f10db63e56e5a3aed134b6060
Furkan Kıraç <fkirac@gmail.com>
2018-04-12 14:48:02 +0300

    Partial template specialization for class templates. Function template specialization.

--- Day8/main.cpp
+++ Day8/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <optional>
-#include <variant>
-
-#include "VariantVisitor.h"
-#include "CustomLiterals.h"
-
-// RVO
-// noexcept operator and specifier
-
-// std::map
-// std::any
+#include <string>
 
 // partial template specialization for classes
 // template specialization for function templates
+
+// std::map
+// std::any
 
 // static_cast, dynamic_cast, reinterpret_cast, const_cast
 // CRTP
@@ -22,43 +15,48 @@
 
 using namespace std;
 
-struct Huge
+template<typename T, int K>
+struct Array
 {
-    int array[10000];
-    double dbl[20000];
-    // ...
-
-    Huge() { cout << "constructor " << this << endl; }
-    ~Huge() { cout << "destructor " << this << endl; }
-    Huge(const Huge&)  { cout << "copy constructor " << this << endl; }
+    T values[K];
 };
 
-Huge func(int i)
+template<int K>
+struct Array<double, K>
 {
-    Huge huge1, huge2;
-    if(i > 0)
-        return huge1;
-    return huge2;
+    double doubles[K];
+    std::string text;
+};
+
+template<>
+struct Array<float, 1>
+{
+    float V;
+};
+
+template<typename T, int K = 1>
+inline void println(T value)
+{
+    cout << value;
+    for(int i=0; i<K; ++i)
+        cout << endl;
 }
+
 
 int main()
 {
-    using namespace Day8;
+    Array<int, 10> A;
+    A.values[0] = 10;
 
-    Huge huge = func(1);
-    std::cout << "bla bla" << endl;
+    Array<double, 20> D;
+    D.doubles[2] = 2.3;
+    D.text = "hurray";
 
-//    auto retval = func(0);
-//    std::cout << retval.value_or(-1) << std::endl;
+    Array<float, 1> F;
+    F.V = 3.2f;
 
-//    {
-//        using namespace VariantVisitor;
-//        run();
-//    }
+    println(F.V);
+    println<std::string, 3>(D.text);
 
-//    {
-//        using namespace CustomLiterals;
-//        run();
-//    }
     return 0;
 }
