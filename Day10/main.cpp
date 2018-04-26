d9f6744f0de9f40d6fe09817485ad700f3f5ae9e
Furkan Kıraç <fkirac@gmail.com>
2018-04-26 15:14:37 +0300

    Compile time integer support with macros.

--- Day10/main.cpp
+++ Day10/main.cpp
@@ -5,6 +5,7 @@
 // template auto parameters
 
 // array types with constant size
+
 // compile time integral numbers
 
 // std::map, std::any
@@ -14,25 +15,42 @@
 
 using namespace std;
 
-//template<bool B, typename T = void>
-//struct enable_if { };
-
-//template<class T>
-//struct enable_if<true, T>
-//{
-//    using type = T;
-//};
-
-template<typename T>
-typename enable_if<!is_same_v<T, string>>::type func(T value)
+template<typename U, U u>
+struct cint
 {
-    cout << value << endl;
-}
+    using value_type = U;
+    static constexpr value_type value = u;
 
+    template<typename V, V v>
+    auto operator +(cint<V, v>)
+    {
+        return cint<decltype(u+v), u+v>{};
+    }
+
+    template<typename V, V v>
+    bool operator ==(cint<V, v>)
+    {
+        return u == v;
+    }
+
+};
+
+#define CINT(type, value) cint<type, value>{}
 
 int main()
 {
-    func(5);
-    func("furkan"s);
-    return 0;
+    auto S = "furkan"s;
+
+//    A::value ---> 5;
+//    A::value_type ---> int
+
+    auto A = CINT(int, 10);
+    auto B = cint<long int, 10>{};
+    auto C = A + B;
+    bool x = A == B;
+//    cout << (x ? "true" : "false") << endl;
+
+//    decltype(C) --> cint<long int, 15>
+
+    return C.value;
 }
