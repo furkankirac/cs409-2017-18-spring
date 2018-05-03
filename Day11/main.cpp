539b839dd2ab32b5fd9a9e0af8a963a2dd908943
Furkan Kıraç <fkirac@gmail.com>
2018-05-03 21:28:39 +0300

    Tidyness.

--- Day11/main.cpp
+++ Day11/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include <variant>
 #include <vector>
 #include <numeric>
-#include <algorithm>
 
 using namespace std;
-
-// std::common_type_t
-// std::gcd, lcm
-// std::stoi, stol, stof
-// std::add_cv, std::add_const, std::add_volatile
-// std::clamp
-
-// std::map
-// std::any
-
-// static_cast, dynamic_cast, reinterpret_cast, const_cast
-
-// inline variables
-// [[fallthrough]], [[maybe_unused]], [[nodiscard]]
 
 int main()
 {
@@ -26,24 +10,12 @@ int main()
 
     vector<float> V{1.1, 2.2, 3.3, 4.9};
 
-    auto sum = accumulate(next(V.begin()), V.end(), to_string(V[0]),
-            [](const string& s, float value) {
-                return s + ", " + to_string(value);
-                }
-    );
+    // start from the string version of the first float and concatenate a comma followed by each float in the array by converting it to string
+    auto sum = accumulate(next(V.begin()), V.end(), to_string(V[0]), [](const string& s, float value) { return s + ", " + to_string(value); });
     cout << sum << endl;
 
     sum.erase(remove(sum.begin(), sum.end(), ','), sum.end());
     cout << sum << endl;
 
-
-    // std::remove, std::remove_if
-
-
     return 0;
 }
-
-
-
-
-
