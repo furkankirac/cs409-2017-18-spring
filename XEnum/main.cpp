7365e3b7f03b1d05cee10fe921646e2b89ae54f4
Furkan Kıraç <fkirac@gmail.com>
2018-05-14 22:43:25 +0300

    ColorType is generalized to get ColorTypeDef as template parameter.

--- XEnum/main.cpp
+++ XEnum/main.cpp
@@ -40,48 +40,50 @@ using namespace compile_time;
 
 #define ENUMINFO(x, y, z) CINT(x), CSTR(y), CSTR(z)
 
-enum class ColorTypeDef { Red, Green, Blue, Orange };
+enum class ColorTypeDef { Red, Green, Blue, Orange, DEFAULT = Red };
 
-template<typename... Ts>
+template<typename ENUMTYPE, typename... Ts>
 struct ColorType
 {
+    using type = ENUMTYPE;
     using TUPLE = std::tuple<Ts...>;
 
-    ColorTypeDef value;
+    ENUMTYPE value;
 
-    constexpr ColorType(const Ts...)
+    constexpr ColorType(const Ts...) : value{ENUMTYPE::DEFAULT}
     {
     }
 
-    template<typename ENUMTYPE, ENUMTYPE ENUMVAL, size_t i = 0>
-    constexpr auto infoOf() const -> EnumInfo
+    constexpr ColorType(ENUMTYPE value, const Ts...) : value{value}
+    {
+    }
+
+    template<ENUMTYPE ENUMVAL, size_t i = 0>
+    static constexpr auto infoOf() -> EnumInfo
     {
         const auto sz = std::tuple_size_v<TUPLE>;
         if constexpr(std::is_same_v<cint<ENUMVAL>, std::tuple_element_t<i, TUPLE>>)
             return { std::tuple_element_t<i+1, TUPLE>::c_str(), std::tuple_element_t<i+2, TUPLE>::c_str() };
         else if constexpr(i < sz-1)
-            return infoOf<ENUMTYPE, ENUMVAL, i+1>();
+            return infoOf<ENUMVAL, i+1>();
         else
             return {"", ""};
     }
-
-    template<auto ENUMVAL>
-    constexpr auto infoOf() const
-    { return infoOf<decltype(ENUMVAL), ENUMVAL>(); }
 };
 
+template<typename FIRST, typename ...REST>
+ColorType(FIRST, const REST...) -> ColorType<typename FIRST::value_type, FIRST, REST...>;
 
 int main()
 {
     using namespace std;
 
     ColorType ct{
+//        ColorTypeDef::Red,
         ENUMINFO(ColorTypeDef::Red, "Red", "R"),
         ENUMINFO(ColorTypeDef::Green, "Green", "G"),
         ENUMINFO(ColorTypeDef::Blue, "Blue", "B")
     };
-
-    ct.value = ColorTypeDef::Red;
 
     cout << ct.infoOf<ColorTypeDef::Green>().fullname << endl;
 //    cout << ct.infoOf<cint<ColorTypeDef::Green>>().shortname << endl;
