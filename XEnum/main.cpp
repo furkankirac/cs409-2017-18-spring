64874e4bbc402e4686118696e5348a6c60428ecf
Furkan Kıraç <fkirac@gmail.com>
2018-05-14 23:26:53 +0300

    Added TransformationType enum.

--- XEnum/main.cpp
+++ XEnum/main.cpp
@@ -38,23 +38,21 @@ namespace compile_time
 
 using namespace compile_time;
 
-#define ENUMINFO(x, y, z) CINT(x), CSTR(y), CSTR(z)
-
-enum class ColorTypeDef { Red, Green, Blue, Orange, DEFAULT = Red };
-
 template<typename ENUMTYPE, typename... Ts>
-struct ColorType
+struct EnumType
 {
     using type = ENUMTYPE;
     using TUPLE = std::tuple<Ts...>;
 
-    ENUMTYPE value;
+    ENUMTYPE value{ENUMTYPE::DEFAULT};
 
-    constexpr ColorType(const Ts...) : value{ENUMTYPE::DEFAULT}
+    constexpr EnumType(const ENUMTYPE value = ENUMTYPE::DEFAULT) : value(value) { }
+
+    constexpr EnumType(const Ts...)
     {
     }
 
-    constexpr ColorType(ENUMTYPE value, const Ts...) : value{value}
+    constexpr EnumType(ENUMTYPE value, const Ts...) : value{value}
     {
     }
 
@@ -72,18 +70,40 @@ struct ColorType
 };
 
 template<typename FIRST, typename ...REST>
-ColorType(FIRST, const REST...) -> ColorType<typename FIRST::value_type, FIRST, REST...>;
+EnumType(FIRST, const REST...) -> EnumType<typename FIRST::value_type, FIRST, REST...>;
+
+// ENUMTYPE definitions
+#define ENUMINFO(x, y, z) CINT(x), CSTR(y), CSTR(z)
+
+// ---[ ColorType
+enum class ColorTypeDef { Red, Green, Blue, Orange, DEFAULT = Blue };
+
+constexpr EnumType color_type{
+    ENUMINFO(ColorTypeDef::Red, "Red", "R"),
+    ENUMINFO(ColorTypeDef::Green, "Green", "G"),
+    ENUMINFO(ColorTypeDef::Blue, "Blue", "B")
+};
+using ColorType = decltype(color_type);
+
+// ---[ Transformation
+enum class TransformationTypeDef { None, Rotate_90, Rotate_180, Rotate_270, Flip_Horizontal, Flip_Vertical, DEFAULT = None };
+
+constexpr EnumType transformation_type{
+    ENUMINFO(TransformationTypeDef::None, "None", "None"),
+    ENUMINFO(TransformationTypeDef::Rotate_90, "Rotate 90 Degrees", "Rot90"),
+    ENUMINFO(TransformationTypeDef::Rotate_180, "Rotate 180 Degrees", "Rot180"),
+    ENUMINFO(TransformationTypeDef::Rotate_270, "Rotate 270 Degrees", "Rot270"),
+    ENUMINFO(TransformationTypeDef::Flip_Horizontal, "Flip Horizontal", "H.Flip"),
+    ENUMINFO(TransformationTypeDef::Flip_Vertical, "Flip Vertical", "V.Flip")
+};
+using TransformationType = decltype(transformation_type);
 
 int main()
 {
     using namespace std;
 
-    ColorType ct{
-//        ColorTypeDef::Red,
-        ENUMINFO(ColorTypeDef::Red, "Red", "R"),
-        ENUMINFO(ColorTypeDef::Green, "Green", "G"),
-        ENUMINFO(ColorTypeDef::Blue, "Blue", "B")
-    };
+    ColorType ct{ColorTypeDef::Green};
+    TransformationType tt{TransformationTypeDef::Rotate_270};
 
     cout << ct.infoOf<ColorTypeDef::Green>().fullname << endl;
 //    cout << ct.infoOf<cint<ColorTypeDef::Green>>().shortname << endl;
